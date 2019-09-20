4c4f9ea049eac4f3005efe8764cd567eb0a381af
Rogue Bell <mattbellantoni3@gmail.com>
2019-09-20 10:31:59 +0200

    working on demo for vuex

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
   </div>
 </template>
+<script>
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "app",
+  components: {
+    ProductListOne,
+    ProductListTwo
+  }
+};
+</script>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,25 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    products: [
+      { name: "troll", price: 20 },
+      { name: "troll2", price: 40 },
+      { name: "troll3", price: 50 },
+      { name: "troll4", price: 20 }
+    ]
+  },
+  getters: {
+    saleProducts: state => {
+      let saleProducts = state.products.map( product => {
+        return {
+          name: "**" + product.name + "**",
+          price: product.price / 2
+        }
+      });
+      return saleProducts;
+    }
+  },
   mutations: {},
   actions: {}
 });
